d493b9effc2c150319703acf5cce717d1b481fc5
Santiago Castillo <sancastillog@gmail.com>
2020-02-05 23:27:39 +0000

    fixing compilation errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
 * main- Entry point
 *
-* Return: always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -15,7 +15,7 @@ float floatSize;
 printf("Size of a char: %ld byte(s)\n", sizeof(charSize));
 printf("Size of an int: %ld byte(s)\n ", sizeof(intSize));
 printf("Size of a long int: %ld byte(s)\n ", sizeof(longIntSize));
-printf("Size of a long long int: %ld byte(s)\n ", sizeof(longLongInt));
-printf("Size of a float: %ld byte(s)\n", sizeof(floatSize);
+printf("Size of a long long int: %ld byte(s)\n ", sizeof(longlongInt));
+ printf("Size of a float: %ld byte(s)\n", sizeof(floatSize));
 return (0);
 }
