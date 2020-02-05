598d0810909dae80ae99a06c4f9e0a557fb08281
Santiago Castillo <sancastillog@gmail.com>
2020-02-05 23:38:00 +0000

    why

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 int intSize;
+char charSize;
 long int longIntSize;
 long long int longlongInt;
-char charSize;
 float floatSize;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(charSize));
 printf("Size of an int: %ld byte(s)\n ", sizeof(intSize));
 printf("Size of a long int: %ld byte(s)\n ", sizeof(longIntSize));
 printf("Size of a long long int: %ld byte(s)\n ", sizeof(longlongInt));
- printf("Size of a float: %ld byte(s)\n", sizeof(floatSize));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatSize));
 return (0);
 }
