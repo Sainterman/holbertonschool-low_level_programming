9d47b16a4dcb29aa7559d206f30c7108e4e67f10
Santiago Castillo <sancastillog@gmail.com>
2020-02-05 23:10:37 +0000

    Pushing task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int intSize;
+long int longIntSize;
+long long int longlongInt;
+char charSize;
+float floatSize;
+
+printf("Size of a char: %ld byte(s)\n", sizeof(charSize));
+printf("Size of an int: %ld byte(s)\n ", sizeof(intSize));
+printf("Size of a long int: %ld byte(s)\n ", sizeof(longIntSize));
+printf("Size of a long long int: %ld byte(s)\n ", sizeof(longLongInt));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatSize);
+}
