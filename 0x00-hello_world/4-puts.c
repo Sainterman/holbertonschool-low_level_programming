63ed7721c007f0f59bed6595f7efccca80f60901
Santiago Castillo <sancastillog@gmail.com>
2020-02-05 20:35:26 +0000

    fixing task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-const char string[] = "\"Programming is like building a multilingual puzzle";
+const char string[] = "\"Programming is like building a multilingual puzzle \n";
 puts(string);
 return (0);
 }
