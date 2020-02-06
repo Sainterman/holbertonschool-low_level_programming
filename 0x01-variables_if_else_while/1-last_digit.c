23ecd8f8a029e0950674b36a5982c8558316d601
Santiago Castillo <sancastillog@gmail.com>
2020-02-06 19:13:25 +0000

    pushing task 0 and 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	printf("Last digit of %d ", n);
+	if (lastDigit > 5)
+		printf("is %d and is grater than 5\n", lastDigit);
+	else if (lastDigit == 0)
+		printf("is %d and is 0\n", lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+		printf("is %d and is less than 6 and not 0\n", lastDigit);
+	else
+		printf("is %d\n", lastDigit);
+	return (0);
+}
