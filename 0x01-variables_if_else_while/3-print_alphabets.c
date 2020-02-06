a5b3159b6854e6913fc2cd19509df0a338daaeb9
Santiago Castillo <sancastillog@gmail.com>
2020-02-06 20:15:56 +0000

    pushing task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		putchar(character);
+	for (character = 'A'; character <= 'Z'; character++)
+		putchar(character);
+	putchar('\n');
+	return (0);
+}
