28eef9601fd837ef8eb997c80c4e9ed360299249
Santiago Castillo <sancastillog@gmail.com>
2020-02-06 00:12:40 +0000

    pushing task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main- Entry point
@@ -7,7 +6,6 @@
  */
 int main(void)
 {
-const char string[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-write(2,string,sizeof(string));
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
 return (1);
 }
