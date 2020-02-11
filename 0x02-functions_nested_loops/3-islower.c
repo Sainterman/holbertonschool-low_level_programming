494081a2475cc7487eba73b45b9253d4dc1534c0
Santiago Castillo <sancastillog@gmail.com>
2020-02-11 14:40:44 +0000

    adding betty descriptions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,9 @@
+/**
+ * _islower - function that checks for lowercase character.
+ * @c: the ASCII decimal value of a letter
+ *
+ * Return: Returns 1 if letter is lowercase and 0 if upper
+ */
 int _islower(int c)
 {
 	if (c <= 122 && c >= 97)
