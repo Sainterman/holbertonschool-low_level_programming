ba64935ef0e7dd84b11a2655f6e9f3cf7313e016
Santiago Castillo <sancastillog@gmail.com>
2020-02-11 14:59:48 +0000

    fixing function description on tasks 4 to 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,9 @@
 /**
  * print_sign - return 1, 0 or -1 for positive, zero or negative
  * integer respectively
+ *
+ * @n: Integer value to be checked for negative or positive sign
+ *
  * Return: 1 for positive int, 0 for integer 0 and -1 for nevative
  */
 int print_sign(int n)
@@ -9,17 +12,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,14 @@
+/**
+ * _abs - check absolute value of int @a
+ *
+ * @a: Integer value to be evaluated
+ *
+ * Return: Returns absolute value of int @a, always positive integer
+ */
 int _abs(int a)
 {
-	if(a >= 0)
-		return(a);
+	if (a >= 0)
+		return (a);
 	else
-		return(a*-1);
+		return (a * -1);
 }
