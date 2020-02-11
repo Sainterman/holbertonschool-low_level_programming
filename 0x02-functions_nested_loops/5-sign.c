d16d2d241db6a3af56a925410bf7d7a5581f7228
Santiago Castillo <sancastillog@gmail.com>
2020-02-11 04:13:56 +0000

    pushing task 5 and 6 and header update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - return 1, 0 or -1 for positive, zero or negative
+ * integer respectively
+ * Return: 1 for positive int, 0 for integer 0 and -1 for nevative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,7 @@
+int _abs(int a)
+{
+	if(a >= 0)
+		return(a);
+	else
+		return(a*-1);
+}
