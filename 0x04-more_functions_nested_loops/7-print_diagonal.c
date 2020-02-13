8ce7cf31113931ce664729f1c5dfd4901330024a
Santiago Castillo <sancastillog@gmail.com>
2020-02-13 21:35:37 +0000

    pushing print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagonal - printing diagonal line
+ *
+ *@n: number of times the \ character shoule be printed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
