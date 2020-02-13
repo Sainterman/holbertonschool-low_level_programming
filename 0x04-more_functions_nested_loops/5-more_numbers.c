9294c6c75d205468c5e5ccc888b705bc0539fa23
Santiago Castillo <sancastillog@gmail.com>
2020-02-13 20:29:12 +0000

    pushing task 5 and header

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * more_numbers - print numbers from 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+	int i,number;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (number = 0;number < 15; number++)
+		{
+			if (number >= 10)
+			{
+				_putchar(number / 10 + '0');
+			}
+			_putchar(number % 10 + '0');
+	       	}
+		_putchar('\n');
+	}
+}
