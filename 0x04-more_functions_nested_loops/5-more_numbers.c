ee45c692432acebec62f46d23724524312875e5f
Santiago Castillo <sancastillog@gmail.com>
2020-02-13 20:33:41 +0000

    pushing task 5 and header

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-	int i,number;
+	int i, number;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (number = 0;number < 15; number++)
+		for (number = 0; number < 15; number++)
 		{
 			if (number >= 10)
 			{
 				_putchar(number / 10 + '0');
 			}
 			_putchar(number % 10 + '0');
-	       	}
+		}
 		_putchar('\n');
 	}
 }
