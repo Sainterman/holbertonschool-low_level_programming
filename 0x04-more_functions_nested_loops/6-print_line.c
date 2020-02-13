664036d975d431ac57ef80d623a93ba425398b79
Santiago Castillo <sancastillog@gmail.com>
2020-02-13 20:55:16 +0000

    pushing tasl 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - print @n underscore characters
+ *
+ *@n: length of underscore line
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
