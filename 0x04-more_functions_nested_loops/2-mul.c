156a96cf6607d07272c3391a4b1be485940428ef
Santiago Castillo <sancastillog@gmail.com>
2020-02-13 19:08:10 +0000

    pushing holberton.h and task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - Multiply @a by @b times
+ *
+ * @a: int to be multiplied by factor @b
+ *
+ * @b: int that multiplies @a
+ *
+ * Return: multiplication of @a and @b integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
