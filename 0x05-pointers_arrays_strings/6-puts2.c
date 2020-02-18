90aff7a70b5834aceeba3ddc03c12e23bdacb1c0
Santiago Castillo <sancastillog@gmail.com>
2020-02-18 22:23:32 +0000

    pushing betty fixes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
+ * puts2 - print every other character of a string
  *
- *
- *
+ * @str: address of the first element of string
  *
  */
 void puts2(char *str)
@@ -10,7 +10,7 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
