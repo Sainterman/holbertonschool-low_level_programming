302351e21eb000abcf7e8179d5a6c55e8284e500
Santiago Castillo <sancastillog@gmail.com>
2020-02-18 20:14:55 +0000

    pushing task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return size of string @s
+ *
+ * @s: address to first char of string
+ *
+ * Return: Size of string that begins on @s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_rev - prints a string in reverse fllowd by new line
+ *
+ * @s: address to first char of string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
