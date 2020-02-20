0e31bf9fc87358e9c3112825e58c751019ce3bcc
Santiago Castillo <sancastillog@gmail.com>
2020-02-20 19:53:59 +0000

    medio loco en el coco

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,11 @@
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 == *s2; ++s1; ++s2
+	int i;
+
+	i = 0;
+
+	for (; s1[i] == s2[i]; i++)
+		if (*s1 == *s2)
+			return (0);
+	return (s1[i] - s2[i]);
 }
