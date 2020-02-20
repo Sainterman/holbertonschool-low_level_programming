42b26cebad373fa85b5cb0e124b6e2a726a1ab03
Santiago Castillo <sancastillog@gmail.com>
2020-02-20 19:03:54 +0000

    pushing task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
 /**
+ * _strncat - concatenate @n bytes from @src to @dest
  *
+ * @src: address of string to be concatenated into @dest
  *
+ * @dest: address of string to be concatenated with @n bytes from @src
  *
+ * @n: number of bytes(characters) to be concatenated form @src into @dest
  *
+ * Return: @dest
  *
  */
 
@@ -19,6 +24,6 @@ char *_strncat(char *dest, char *src, int n)
 		dest[i + j] = src[j];
 	dest[i + j] = '\0';
 
-	return(dest);
+	return (dest);
 
 }
