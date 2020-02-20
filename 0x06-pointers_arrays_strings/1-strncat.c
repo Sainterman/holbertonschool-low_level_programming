c55876605b78a585bb10fa78162d8d6b6cecbe0c
Santiago Castillo <sancastillog@gmail.com>
2020-02-20 18:58:04 +0000

    pushing task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+
+	for (j = 0 ; j < n && src[j] != '\0' ; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+
+	return(dest);
+
+}
