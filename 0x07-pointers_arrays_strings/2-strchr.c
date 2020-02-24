968b56965138dec79e0f5dbb47920e41cd6f05c4
Santiago Castillo <sancastillog@gmail.com>
2020-02-24 21:36:31 +0000

    pushing task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+ * _strchar - locates a character in a string.
+ *
+ * @s: pointer to first char on string
+ * @c: char to be found in @s string
+ *
+ * Return: pointer to the first occurence of character in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	if (s[i] == '\0')
+		return (&s[i]);
+	else
+		return ('\0');
+}
