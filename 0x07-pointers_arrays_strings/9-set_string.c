9261d2440d6c116644e32c3030e1b7c3938bdc0b
santiago <sancastillog@gmail.com>
2020-02-25 04:58:27 +0000

    pusing taks 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - that sets the value of a pointer to a char.
+ *
+ * @s: pointer to pointer of string 
+ * @to: pointer to string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
