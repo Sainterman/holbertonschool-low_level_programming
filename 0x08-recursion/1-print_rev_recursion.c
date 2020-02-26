a72e26e60c2181c586a589ce5b5ed1885e72498a
Santiago Castillo <sancastillog@gmail.com>
2020-02-26 22:23:42 +0000

    pushing task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
+/**
+ *
+ *
+ *
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
+		_print_rev_recursion((void *)(s + 1));
 	}
 	_putchar(*s);
 }
