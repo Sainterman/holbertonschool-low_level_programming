539bb7e988342f24c9eb851a3b2783797960b674
santiago <sancastillog@gmail.com>
2020-02-28 04:52:48 +0000

    pushing task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,11 @@
+/**
+ * test - guess sqrt
+ *
+ * @s: number to find sqrt
+ * @j: guess
+ *
+ * Return: sqrt of s
+ */
 int test(int s, int j)
 {
 	if (j * j == s)
@@ -10,6 +18,12 @@ int test(int s, int j)
 		return (test(s, j++));
 	}
 }
+/**
+ * _sqrt_recursion - calculates square rtoot of integer number if rational sqrt
+ * @n:integer to calculate square root from
+ *
+ * Return: sqrt of n or -1 if not possible
+ */
 int _sqrt_recursion(int n)
 {
 	int y = 0;
