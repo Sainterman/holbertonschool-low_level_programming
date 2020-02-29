ad86c96ece648d4288bda4719d0dd781131cd33c
santiago <sancastillog@gmail.com>
2020-02-29 22:05:41 +0000

    pusjing betty fixe

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - print first argument passed to program
+ * @argc: int unused
+ * @argv: pointer to char holding arguments of the program
+ * Return: always 0
+ */
 int main(__attribute__((unused)) int argc,  char *argv[])
 {
 	printf("%s\n", argv[0]);
