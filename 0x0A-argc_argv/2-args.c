f997e7debb1d09e8a6cab206ca8ad2b1adba2802
santiago <sancastillog@gmail.com>
2020-02-29 23:13:33 +0000

    pushing task 2 working remote

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - print all arguments the program recieves
  * @argc: integer number of arguments
  * @argv: pointer to first char in string of argments
@@ -8,6 +8,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 
