ed6b82f437a76ac1ec8b2eaff093acbfc0373754
Santiago Castillo <sancastillog@gmail.com>
2020-03-02 23:43:50 +0000

    pushing task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - copy string in src to dest
+ *
+ * @dest: pointer where to copy string @src
+ * @src: address of string to copy
+ * Retun: @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*src)
+	{
+		*(dest + i) = src;
+		i++;
+		src++;
+	}
+
+	return (dest);
+}
