5c93d3b1372d8a2ed58f219b243292e0d7a546ef
Santiago Castillo <sancastillog@gmail.com>
2020-03-03 00:02:17 +0000

    pushing fix to9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	if (src[0] == '\0')
+		dest[0] = '\0';
+
+	while (src[i] != '\0')
 	{
-		*(dest + i) = src[i];
+		dest[i] = src[i];
 		i++;
 	}
 
