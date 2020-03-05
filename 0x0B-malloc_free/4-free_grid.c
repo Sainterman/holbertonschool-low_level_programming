27bd56cb9910e2017fb2e21041add82df5d1b498
Santiago Castillo <sancastillog@gmail.com>
2020-03-05 00:37:52 +0000

    pushing the task number four working properly in valgrind

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,12 +10,9 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (height <= 0)
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
+		free(grid[i]);
 	}
+	free(grid);
 }
