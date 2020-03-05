a49b1aca9295da0b400cafe87c5e2f10f061c874
Santiago Castillo <sancastillog@gmail.com>
2020-03-05 00:31:54 +0000

    pushing task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+/**
+ * free_grid - free 2 dimensional grid previousluy created by alloc_grid
+ * @grid: address to first in grid
+ * @height: height of grid
+ * Return: void
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
