6ff06b7486754776a5efbfd78144d0241b295cc2
Santiago Castillo <sancastillog@gmail.com>
2020-03-05 18:48:34 +0000

    pushing header ant task 0 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Prototype: void *malloc_checked(unsigned int b);
+ * Returns a pointer to the allocated memory
+ * if malloc fails, the malloc_checked
+ * function should cause normal process termination with a status value of 98
+ *
+ * @b: number of bytes to be allocated
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *allocatedmeme;
+
+	allocatedmeme = malloc (b);
+	if (allocatedmeme == NULL)
+		exit (98);
+
+	return (allocatedmeme);
+}
