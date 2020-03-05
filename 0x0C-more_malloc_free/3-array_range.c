3d735e40dcf54e9713ae69ee6835e2cf231c8916
Santiago Castillo <sancastillog@gmail.com>
2020-03-05 22:15:35 +0000

    pushing task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * array_range - reates an array of integers.
+ * The array created should contain all the values from min (included)
+ * to max (included), ordered from min to max
+ * @min: minimum range
+ * @max:  maximim number in array bro
+ * Return: address of ints array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, size, i;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min;
+	array = malloc((max - min) * sizeof(int) + 1);
+
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i <= size; i++)
+	{
+		array[i] = min;
+		min++;
+	}
+	return (array);
+}
