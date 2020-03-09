9e1f0173c091b36b88d4d9b73ccf9e8127d5b083
Santiago Castillo <sancastillog@gmail.com>
2020-03-09 22:25:22 +0000

    pushing task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a dog struct
+ *
+ * @d: Address of dog struct
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			d->name = "nil";
+		if (d->owner == NULL)
+			d->owner = "nil";
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+		return;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,6 @@ struct dog
 	char *owner;
 	float age;
 };
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
