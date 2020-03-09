9b9818b298c2510786f8beabfaf7ee2fdfa3be07
Santiago Castillo <sancastillog@gmail.com>
2020-03-09 22:51:20 +0000

    pushing task 2 fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 /**
  * print_dog - prints a dog struct
  *
  * @d: Address of dog struct
  *
- *
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
@@ -14,8 +13,10 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "nil";
+
 		if (d->owner == NULL)
 			d->owner = "nil";
+
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
