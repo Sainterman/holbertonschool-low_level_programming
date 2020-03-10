eba9225edc81d72457d4f92f15a2a38a04da7da0
santiago <sancastillog@gmail.com>
2020-03-10 03:53:07 +0000

    pushing task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: address of striong to copy
+ *
+ * Return: address of duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i = 0, j = 0;
+
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	duplicate = (char *) malloc(1 + sizeof(char) * i);
+	if (!duplicate)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+	{
+		duplicate[j] = str[j];
+	}
+	return (duplicate);
+}
+/**
+ * new_dog - create a new variable type dog_t
+ * @name: Address of name string
+ * @age: age of the dog
+ * @owner: addtess of owners name
+ * Return: address of type dog_t element
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+		return (NULL);
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
+		return (NULL);
+	return (new_dog);
+}
