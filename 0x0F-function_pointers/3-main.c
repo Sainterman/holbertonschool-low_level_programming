2efe539e72dd382ee2346dd5dfbf335c8507747c
santiago <sancastillog@gmail.com>
2020-03-12 04:40:04 +0000

    pushing fixes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operation = get_op_fun(argv[2]);
+	operation = get_op_func(argv[2]);
 	if (operation == NULL)
 	{
 		printf("Error\n");
