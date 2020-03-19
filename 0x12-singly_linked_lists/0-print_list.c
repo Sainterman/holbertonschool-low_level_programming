ab1451542ca7dadd2f9a54a951905c2e2e0ce26a
santiago <sancastillog@gmail.com>
2020-03-19 19:54:50 +0000

    fixed trailing whitespace

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,6 @@
 /**
  * print_list - function that prints all the elements of a list_t list.
  * @h: pointer to head node of list_t
- * 
  * Return: Number of nodes
  */
 size_t print_list(const list_t *h)
