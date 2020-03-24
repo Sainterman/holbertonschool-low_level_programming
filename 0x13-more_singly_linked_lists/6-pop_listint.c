16f511df0c60dbdaccceaf1e0126465d67d52bb1
Santiago Castillo <sancastillog@gmail.com>
2020-03-24 20:59:04 +0000

    pushin task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data = 0;
+
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	data = temp->n;
+	(*head) = (*head)->next;
+	free (temp);
+	return (data);
+}
