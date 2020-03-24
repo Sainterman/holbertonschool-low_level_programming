942e28048294e1d35693433693fdfac26e01fec5
Santiago Castillo <sancastillog@gmail.com>
2020-03-24 17:01:09 +0000

    pushint task from 0 - 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,12 @@
+#include  "lists.h"
+/**
+* listint_len - returns the number of elements in a linked listint_t list.
+* @h: Return
+*
+*/
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+		return 0;
+	return (1 + listint_len(h->next));
+}
