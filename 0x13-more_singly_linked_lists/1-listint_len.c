fc6b6301ee855dd8f054a3d9956b648075d58963
Santiago Castillo <sancastillog@gmail.com>
2020-03-24 17:40:10 +0000

    betty return fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,8 @@
 #include  "lists.h"
 /**
 * listint_len - returns the number of elements in a linked listint_t list.
-* @h: Return
-*
+* @h: address of listint_t head
+* Return: number of node in the list
 */
 size_t listint_len(const listint_t *h)
 {
