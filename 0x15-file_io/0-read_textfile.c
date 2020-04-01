0ccb96ebb51097c2c3350d2309fd765bdfc88da8
Santiago Castillo <sancastillog@gmail.com>
2020-04-01 02:02:10 +0000

    pushing working shit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,21 +8,21 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, count, readChars, i;
-	char *aux = malloc(sizeof(char) * (letters));
+	char *aux = malloc(sizeof(char) * (letters + 1));
 
-	if (aux == NULL)
-		return (0);
-	if (filename == NULL)
+	if (aux == NULL || filename == NULL || letters == 0)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
+		close(fd);
 		free(aux);
 		return (0);
 	}
-	readChars = read(fd, (void *)aux, (letters));
+	readChars = read(fd, (void *)aux, letters);
 	if (readChars == -1)
 	{
+		close(fd);
 		free(aux);
 		return (0);
 	}
@@ -31,9 +31,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		count = write(STDOUT_FILENO, aux + i, 1);
 		if (count == -1)
 		{
+			close(fd);
 			free(aux);
 			return (0);
 		}
 	}
+	close(fd);
 	return (i);
 }
